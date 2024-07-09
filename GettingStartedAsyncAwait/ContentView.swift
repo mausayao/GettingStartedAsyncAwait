5c7779dbec529ccd73b1a0e6071ad4d370b48302
mausayao <mauricio.sayao@gmail.com>
2024-07-09 10:16:24 -0400

    initial commit

--- GettingStartedAsyncAwait/ContentView.swift
+++ GettingStartedAsyncAwait/ContentView.swift
@@ -7,10 +7,30 @@
 
 import SwiftUI
 
+struct CurrentDate: Decodable, Identifiable {
+    let id = UUID()
+    let date: String
+    
+    private enum CodingKeys: String, CodingKey {
+        case date = "date"
+    }
+}
+
 struct ContentView: View {
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(1...10, id: \.self) { index in
+                Text("\(index)")
+            }.listStyle(.plain)
+            
+            .navigationTitle("Dates")
+            .navigationBarItems(trailing: Button(action: {
+                // button action
+            }, label: {
+                Image(systemName: "arrow.clockwise.circle")
+            }))
+        }
     }
 }
 
